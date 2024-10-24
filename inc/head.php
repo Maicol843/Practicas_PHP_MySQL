9f791765b6fabafd5ed47100374a48d202445b6c
Maicol D. Mamani Chalco <mdmch13@gmail.com>
2024-10-23 23:07:05 -0300

    Se creo el navegador del proyecto

---
+++ inc/head.php
@@ -0,0 +1,8 @@
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Título de la página-->
+    <title>Sistema de Inventario</title>
+    <!--Bulma CSS-->
+    <link rel="stylesheet" href="./css/bulma.min.css">
+    <!--Estilo CSS -->
+    <link rel="stylesheet" href="./css/estilos.css">
--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--Título de la página-->
-    <title>Sistema de Inventario</title>
+    <?php include "./inc/head.php";?>
 </head>
 <body>
-    
+    <?php include "./inc/navbar.php";?>
+    <?php include "./inc/script.php";?>
 </body>
 </html>
