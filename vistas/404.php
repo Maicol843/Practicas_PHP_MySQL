1d9d58d4fe4126577d98f1a5f13b3dcab80e451c
Maicol D. Mamani Chalco <mdmch13@gmail.com>
2024-10-31 16:44:00 -0300

    Creando vistas de Login, Home y página 404

--- vistas/404.php
+++ vistas/404.php
@@ -1,1 +1,6 @@
-404
+<div class="main-container">
+    <section class="hero-body">
+        <p class="title">Error 404</p>
+        <div class="subtitle">Página no encontrada</div>
+    </section>
+</div>
