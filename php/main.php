a511fecb106eebea3572efe9213bb75ccb93339f
Maicol D. Mamani Chalco <mdmch13@gmail.com>
2024-10-31 18:16:04 -0300

    Función PHP para evitar inyección SQL

--- php/main.php
+++ php/main.php
@@ -13,4 +13,35 @@
             return true;
         }
     }
+
+    # Limpiar cadenas de texto #
+    function limpiar_cadena($cadena){
+        $cadena = trim($cadena); # trim: elimina espacio en blanco (u otro tipo de caracteres) del inicio y el final de la cadena #
+        $cadena = stripslashes($cadena); # stripslashes: quita las barras de un string con comillas escapadas #
+        $cadena = str_ireplace("<script>", "", $cadena); # str_ireplace: reemplaza texto mediante una búsqueda. Versión insensible a mayúsculas y minúsculas #
+        $cadena = str_ireplace("</script>", "", $cadena);
+        $cadena = str_ireplace("<script src", "", $cadena);
+        $cadena = str_ireplace("<script type=", "", $cadena);
+        $cadena = str_ireplace("SELECT * FROM", "", $cadena);
+        $cadena = str_ireplace("DELETE FROM", "", $cadena);
+        $cadena = str_ireplace("INSERT INTO", "", $cadena);
+        $cadena = str_ireplace("DROP TABLE", "", $cadena);
+        $cadena = str_ireplace("DROP DATABASE", "", $cadena);
+        $cadena = str_ireplace("TRUNCATE TABLE", "", $cadena);
+        $cadena = str_ireplace("SHOW TABLES;", "", $cadena);
+        $cadena = str_ireplace("SHOW DATABASES;", "", $cadena);
+        $cadena = str_ireplace("<?php", "", $cadena);
+        $cadena = str_ireplace("?>", "", $cadena);
+        $cadena = str_ireplace("--", "", $cadena);
+        $cadena = str_ireplace("^", "", $cadena);
+        $cadena = str_ireplace("<", "", $cadena);
+        $cadena = str_ireplace("[", "", $cadena);
+        $cadena = str_ireplace("]", "", $cadena);
+        $cadena = str_ireplace("==", "", $cadena);
+        $cadena = str_ireplace(";", "", $cadena);
+        $cadena = str_ireplace("::", "", $cadena);
+        $cadena = trim($cadena); 
+        $cadena = stripslashes($cadena);
+        return $cadena;
+    }
 ?>
